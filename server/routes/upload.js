27e208a5438a5ced104781e86b02beab9e0e531f
Oldahir Gomez <oldahir_gomez25@hotmail.com>
2019-11-30 22:59:31 -0600

    Subida de archivos

---
+++ server/routes/upload.js
@@ -0,0 +1,158 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+const Usuario = require('../models/usuario');
+const Producto = require('../models/producto');
+
+const fs = require('fs');
+const path = require('path');
+
+// default options
+app.use(fileUpload({ useTempFiles: true }));
+
+app.put('/upload/:tipo/:id', (req, res) => {
+
+    let tipo = req.params.tipo;
+    let id = req.params.id;
+
+    if (!req.files) {
+        return res.status(400)
+            .json({
+                ok: false,
+                err: {
+                    message: 'No se ha seleccionado ningun archivo'
+                }
+            });
+    }
+
+    //Validar tipo
+    let tiposValidos = ['productos', 'usuarios'];
+    if (tiposValidos.indexOf(tipo) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Los tipos permitidos son: ' + tiposValidos.join(','),
+            }
+        });
+    }
+
+    let archivo = req.files.archivo;
+    let nombreCortado = archivo.name.split('.');
+    let extencion = nombreCortado[nombreCortado.length - 1];
+
+    // Extenciones permitidas
+    let extencionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
+
+    if (extencionesValidas.indexOf(extencion) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Las extenciones permitidas son: ' + extencionesValidas.join(','),
+                ext: extencion
+            }
+        });
+    }
+
+    //Cambiar el nombre del archivo
+    let nombreArchivo = `${id} - ${new Date().getMilliseconds()}.${extencion}`;
+
+    // Use the mv() method to place the file somewhere on your server
+    archivo.mv(`uploads/${tipo}/${nombreArchivo}`, function(err) {
+        if (err)
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+
+        //Aqui, la imagen carga
+        if (tipo === 'usuarios') {
+            imagenUsuario(id, res, nombreArchivo);
+        } else {
+            imagenProdcto(id, res, nombreArchivo);
+        }
+    });
+});
+
+function imagenUsuario(id, res, nombreArchivo) {
+
+    Usuario.findById(id, (err, usuarioDB) => {
+        if (err) {
+            borrarArchivo(nombreArchivo, 'usuarios');
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioDB) {
+            borrarArchivo(nombreArchivo, 'usuarios');
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no existe'
+                }
+            });
+        }
+
+        borrarArchivo(usuarioDB.img, 'usuarios');
+
+        usuarioDB.img = nombreArchivo;
+
+        usuarioDB.save((err, usuarioGuardado) => {
+
+            res.json({
+                ok: true,
+                usuario: usuarioGuardado,
+                img: nombreArchivo
+            });
+        });
+    });
+}
+
+function imagenProdcto(id, res, nombreArchivo) {
+
+    Producto.findById(id, (err, productoDB) => {
+
+        if (err) {
+            borrarArchivo(nombreArchivo, 'productos');
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!productoDB) {
+            borrarArchivo(nombreArchivo, 'productos');
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no existe'
+                }
+            });
+        }
+
+        borrarArchivo(productoDB.img, 'productos');
+        productoDB.img = nombreArchivo;
+
+        productoDB.save((err, productoGuardado) => {
+
+            res.json({
+                ok: true,
+                producto: productoGuardado,
+                img: nombreArchivo
+            });
+        });
+    });
+}
+
+function borrarArchivo(nombreImagen, tipo) {
+
+    let pathImage = path.resolve(__dirname, `../../uploads/${tipo}/${nombreImagen}`);
+    if (fs.existsSync(pathImage)) {
+        fs.unlinkSync(pathImage);
+    }
+}
+
+
+module.exports = app;
